373936a559a8c8e260318455d9890ee7b6d7a0a3
gauma1831776 <gauma@etu.cegepjonquiere.ca>
2018-11-01 11:07:07 -0400

    Exerecice sur les premiers algoritmes (JS) Exercice Parmis et Composés

--- Exercices Js/Exercices  - Choisir Parmi/JS/traitement14.js
+++ Exercices Js/Exercices  - Choisir Parmi/JS/traitement14.js
@@ -7,19 +7,19 @@ function btnCalculer_onclick()
     Operateur=(document.getElementById("txtOperateur").value);
 
 
-    if(Operateur=="*")
-    {
-        Total=Nbr1*Nbr2
-    }
-    else if(Operateur=="/"){
-        Total=Nbr1/Nbr2
-    }
-    else if(Operateur=="+"){
-        Total=Nbr1+Nbr2
-    }
-    else{
-        Total=Nbr1-Nbr2
-    }
+  switch(Operateur)
+  {
+      case "*": Total=Nbr1*Nbr2;
+          break;
+      case "/": Total=Nbr1/Nbr2;
+          break;
+      case "+":Total=Nbr1+Nbr2;
+      break;
+      case "-": Total=Nbr1-Nbr2;
+      break;
+      default: Operateur="Entrez une opération (-,+,/,*)";
+          break;
+  }
     console.log("le total avec l'operateur "+Operateur+" est de: "+Total);
     document.getElementById("lblMessage").innerHTML="le total avec l'operateur "+Operateur+" est de: "+Total;
 }
