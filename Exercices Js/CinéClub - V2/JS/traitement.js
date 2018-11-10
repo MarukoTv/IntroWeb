a087c348586eb231a43be2b4d45f47a82f942707
gauma1831776 <gauma@etu.cegepjonquiere.ca>
2018-11-10 11:49:34 -0500

    Exerecice sur les premiers algoritmes (JS) Exercice Semaine 11

--- Exercices Js/CinéClub - V2/JS/traitement.js
+++ Exercices Js/CinéClub - V2/JS/traitement.js
@@ -1,51 +1,65 @@
+var PrixBase,Age,ChoixFilm,Total,Rabais,Heure;
 function btnCalculer_onclick()
-{ 
- 	var PrixBase,Age,ChoixFilm,Total,Rabais,Heure;
+{
+    saisirInfo();
+    conditionInfo();
+    calculInfo();
 
+}
+function saisirInfo()
+{
     ChoixFilm = document.getElementById("lstFilm").value;
-   PrixBase  = parseFloat(document.getElementById("txtBasePrix").value);
+    PrixBase  = parseFloat(document.getElementById("txtBasePrix").value);
     Age = parseFloat(document.getElementById("txtAge").value);
-
-
+}
+function conditionInfo()
+{
     if(Age<=16 &&  document.getElementById("chkSession").checked == true )
     {
-    if(  document.getElementById("radMidi").checked == true)
+        if(  document.getElementById("radMidi").checked == true)
         {
             Heure ="midi";
-        Rabais=0.40;
+            Rabais=0.40;
         }
         else
         {
             Heure="soir";
-        Rabais=0.60;
+            Rabais=0.60;
         }
 
     }
-     else if(Age>16 &&  document.getElementById("chkSession").checked == true)
-     {
+    else if(Age>16 &&  document.getElementById("chkSession").checked == true)
+    {
         if( document.getElementById("radMidi").checked == true)
         {
             Heure ="midi";
-      Rabais=0.70;
+            Rabais=0.70;
         }
-         else{
+        else{
             Heure="soir";
 
         }
 
     }
-      else if( document.getElementById("chkSession").checked == true)
+    else if( document.getElementById("chkSession").checked == true)
     {
         Rabais=1;
         Heure ="midi";
     }
-      else {
+    else {
         Rabais=1;
         Heure="soir";
     }
-
+}
+function calculInfo()
+{
     Total= PrixBase*Rabais;
 
-    console.log("Le total es de "+ Total.toFixed(2)+"$"+  " pour le film " + ChoixFilm + " qui est le " + Heure +" et qui est âgé de " + Age );
+    console.log("Le total es de "+ Total.toFixed(2)+"$"+  " pour le film " + ChoixFilm + " qui est à " + Heure +" et qui est âgé de " + Age );
+
+}
+function btnDeroulant_onchange1(num)
+{
+    document.getElementById("photo").src = "img/"+num+".jpg";
 
 }
--- Exercices Js/Exercice _ Joueurs/JS/traitement.js
+++ Exercices Js/Exercice _ Joueurs/JS/traitement.js
@@ -1,54 +1,127 @@
-var Nom,Points,moyenne,i,meilleurPointage;
-var tabPoint = new Array(nombreNom);
-var tabJoueur = new Array(nombreNom);
+var Nom,Points;
+var cptNom=0;nombrePoint=0;
+var tabPoint = new Array(4);
+var tabJoueur = new Array(4);
 function btnRechercher_onclick()
 {
-    calculerJoueurs();
+     rechercherJoueur();
 }
 function  btnAjouter_onclick()
 {
-
+    saisirInformation();
+    calculerJoueurs();
+    ajouterCondition();
+    changerJoueur();
 }
 function btnTrouverMoy_onclick()
 {
-
+    saisirMoyenne();
 }
 function btnTrouverMeilleur_onclick()
 {
 
+    SaisirMeilleurPointage();
 }
 function btnTrouverPire_onclick()
 {
 
+    SaisirPirePointage();
 }
 function saisirInformation()
 {
-    Nom=Document.getElementById("btnRechercher").value;
+    Nom=document.getElementById("txtNom").value;
     Points= parseInt(document.getElementById("txtPoints").value);
 }
 function calculerJoueurs()
 {
-    nombreNom+=1;
-    document.getElementById("lblJoueur").innerHTML="Le nom du joueur est " + nombreNom;
+    cptNom++;
+    tabJoueur[cptNom-1]=Nom;
+    tabPoint[cptNom-1]=Points;
 }
 function saisirMoyenne ()
 {
-for(i=0;i<tabPoint.length;i++)
-{
-moyenne+=Points;
-}
-moyenne=moyenne/tabPoint.length;
+    var i,moyenne=0;
+    for(i=0;i<tabPoint.length;i++)
+    {
+        moyenne+=tabPoint[i];
+    }
+    moyenne=moyenne/tabPoint.length;
     document.getElementById("lblReponse").innerHTML="La moyenne est de  " + moyenne;
 }
 function SaisirMeilleurPointage()
 {
+    var i,meilleurJoueur,meilleurPointage;
+
+    var max=0;
+
     for(i=0;i<tabPoint.length;i++)
     {
-      if(i<=tabPoint[i])
+      if(max<=tabPoint[i])
       {
+          max=tabPoint[i];
           meilleurPointage=tabPoint[i];
+          meilleurJoueur=tabJoueur[i];
 
       }
     }
-    document.getElementById("lblReponse").innerHTML="Le meilleur pointage est de  " + meilleur + "réalisé par "+ nom;
+    document.getElementById("lblReponse").innerHTML="Le meilleur pointage est de  " + meilleurPointage + " réalisé par "+meilleurJoueur;
+}
+function ajouterCondition() {
+
+    if ( cptNom>=0&&cptNom>3)
+    {
+        document.getElementById("btnAjouter").disabled = true;
+        document.getElementById("btnRechercher").disabled = false;
+        document.getElementById("btnTrouverMoy").disabled = false;
+        document.getElementById("btnTrouverMeilleur").disabled = false;
+        document.getElementById("btnTrouverPire").disabled = false;
+    }
+}
+
+function SaisirPirePointage()
+{
+    var i,pireJoueur,pirePoints;
+
+    var pire=100;
+
+    for(i=0;i<tabPoint.length;i++)
+    {
+        if(pire>=tabPoint[i])
+        {
+            pire=tabPoint[i];
+            pirePoints=tabPoint[i];
+            pireJoueur=tabJoueur[i];
+        }
+
+    }
+    document.getElementById("lblReponse").innerHTML="Le pire pointage est de  " +pirePoints + " réalisé par "+ pireJoueur;
+}
+function changerJoueur()
+{
+    document.getElementById("lblNbreJoueur").innerHTML="Numéro du joueur " +(cptNom+1);
+}
+function rechercherJoueur()
+{
+    var trouver=false;
+    var Nom=document.getElementById("txtNom").value;
+    cptNom=0;
+    while(trouver==false && cptNom<=3)
+    {
+        if (tabJoueur[cptNom]==Nom)
+        {
+            trouver=true;
+        }
+        else
+        {
+            cptNom++;
+        }
+    }
+      if(trouver==true)
+      {
+          document.getElementById("lblReponse").innerHTML=tabJoueur[cptNom]+" a obtenu un pointage de "+tabPoint[cptNom]+" points ";
+      }
+      else
+      {
+          document.getElementById("lblReponse").innerHTML="Aucun nom ne correspond ";
+      }
 }
